11e13d76a217c63357c09fab8ac35a520363e366
Jean Pierre Dupouy <jpdupouyc@gmail.com>
2026-05-06 23:32:02 -0400

    init: optimizador de publicaciones Mevak

--- run_hunting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export ANTHROPIC_API_KEY=$(grep ANTHROPIC_API_KEY ~/.zshrc | cut -d'"' -f2)
-export TAVILY_API_KEY=$(grep TAVILY_API_KEY ~/.zshrc | cut -d'"' -f2)
-export ACUAIA_EMAIL_PASSWORD=$(grep ACUAIA_EMAIL_PASSWORD ~/.zshrc | cut -d'"' -f2)
-cd /Users/jeanpierredupouycerda/mi-agente
-/usr/bin/python3 hunting_mevak_v3.py
